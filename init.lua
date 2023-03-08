68d4341c62e28ae3dfc0f09e4fe66ab028e8e0d6
vojtechj <38255592+vojtechj@users.noreply.github.com>
2023-03-08 17:20:21 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -18,6 +18,9 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.hlsearch = false
 
+vim.o.grepprg = [[rg --vimgrep --smart-case $*]]
+vim.o.grepformat = '%f:%l:%c:%m'
+
 -- mappings
 vim.g.mapleader = " "
 
@@ -35,3 +38,26 @@ map("n", "<leader><leader>", ":set hlsearch!<CR>")
 
 map("c", "<c-p>", "<up>")
 map("c", "<c-n>", "<down>")
+
+map("n", "<leader>g", ":sil lgrep! ")
+map("n", "<leader>r", ":sil lgrep! <c-r><c-w> ")
+
+map("n", "<leader>o", ":lopen<CR>")
+map("n", "<leader>c", ":lclose<CR>")
+map("n", "<c-p>", ":lprev<CR>")
+map("n", "<c-n>", ":lnext<CR>")
+
+map("n", "<leader>h", "<c-w>h")
+map("n", "<leader>l", "<c-w>l")
+map("n", "<leader>j", "<c-w>j")
+map("n", "<leader>k", "<c-w>k")
+
+map("n", "<leader>=", "10<c-w>+")
+map("n", "<leader>-", "10<c-w>-")
+map("n", "<leader>.", "10<c-w>>")
+map("n", "<leader>,", "10<c-w><")
+
+map("n", "<leader>;", ":")
+
+map("n", "gk", "gg")
+map("n", "gj", "G")

150e7c71c8dc157bb8f12cfa73ebe34b058c3495
Vojtech Jeso <vojtech.jeso@gmail.com>
2023-09-25 14:46:49 +0200

    syntax highlighting for numbers turned off

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,6 +6,11 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			require('solarized').setup({
+				styles = {
+					numbers = { fg = 'NONE', bg = 'NONE' },
+				},
+			})
 			-- load the colorscheme here
 			vim.cmd([[colorscheme solarized]])
 		end,

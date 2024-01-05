e76dcc20323f5876805499f9362082cf012c7e06
Vojtech Jeso <vojtech.jeso@gmail.com>
2024-01-05 09:25:51 +0100

    mapping for alternate file switching changed

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -41,7 +41,7 @@ vim.keymap.set("n", "<a-,>", "10<c-w><")
 vim.keymap.set("n", "<leader>;", ":")
 vim.keymap.set("n", "<leader>u", ":up<CR>")
 
-vim.keymap.set("n", "<leader>6", ":e #<CR>")
+vim.keymap.set("n", "<leader>a", ":e #<CR>")
 
 vim.keymap.set("n", "gk", "gg")
 vim.keymap.set("n", "gj", "G")
